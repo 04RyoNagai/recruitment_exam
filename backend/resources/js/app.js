ced1ebd12ff49c7070b3e413ecd7f8c52ac59dcb
nagairy <nagairy@star10star.com>
2022-01-13 14:47:15 +0900

    Implement vuejs

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+import './bootstrap'
+import Vue from 'vue'
+import Sample from './components/Sample'
+
+Vue.component('sample-component',require('./components/Sample.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        Sample
+    }
+});
