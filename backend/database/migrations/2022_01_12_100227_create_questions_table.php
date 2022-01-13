3ca83911065f1c2da4af0fc15e767e35a9df4698
nagairy <nagairy@star10star.com>
2022-01-13 11:13:45 +0900

    Fix questions table migration

--- backend/database/migrations/2022_01_12_100227_create_questions_table.php
+++ backend/database/migrations/2022_01_12_100227_create_questions_table.php
@@ -16,13 +16,14 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('set_id');
-            $table->string('title', 255);
+            $table->string('title', 255)->nullable();
             $table->string('correct', 255);
             $table->string('uncorrect1', 255);
             $table->string('uncorrect2', 255)->nullable();
             $table->string('uncorrect3', 255)->nullable();
             $table->string('uncorrect4', 255)->nullable();
             $table->string('uncorrect5', 255)->nullable();
+            $table->string('image_name', 255)->nullable();
             $table->timestamp('deleted_at');
             $table->timestamps();
 
